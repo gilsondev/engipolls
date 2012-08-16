d7af1f1f2b7ec7ac51f2088174e58ec19a5eafdf
Gilson Filho <contato@gilsondev.com>
2012-08-16 07:44:41 -0300

    Inserindo o unipath

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,8 @@
-# Django settings for src project.
+# -*- coding: utf8 -*-
+
+from unipath import Path
+
+PROJECT_DIR = Path(__file__).parent
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                       # Or path to database file if using sqlite3.
+        'USER': '',                       # Not used with sqlite3.
+        'PASSWORD': '',                   # Not used with sqlite3.
+        'HOST': '',                       # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
     }
 }
 
