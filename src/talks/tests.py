d32c13319ff233f81624855e96da53f8ae1c337a
Gilson Filho <contato@gilsondev.com>
2012-08-16 08:52:09 -0300

    Refatorando o test_get_talk_form

--- src/talks/tests.py
+++ src/talks/tests.py
@@ -1,10 +1,11 @@
 # -*- coding: utf8 -*-
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
 class TalkUrlTest(TestCase):
     def test_get_talk_form(self):
         '''Retorna status 200 da lista de palestras'''
-        response = self.client.get('/palestras/')
+        response = self.client.get(reverse('home'))
         self.assertEquals(response.status_code, 200)
