2629dcf55ee259a40913ba588beb86a0f823bde8
Gilson Filho <contato@gilsondev.com>
2012-08-16 07:53:09 -0300

    Configurando dj-database-url'

--- src/settings.py
+++ src/settings.py
@@ -13,16 +13,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                       # Or path to database file if using sqlite3.
-        'USER': '',                       # Not used with sqlite3.
-        'PASSWORD': '',                   # Not used with sqlite3.
-        'HOST': '',                       # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(
+    default='sqlite:///' + PROJECT_DIR.child('engipolls.db'))
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
