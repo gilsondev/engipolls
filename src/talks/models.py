211dd73e6699a614331b324188d9fe7867b03b09
Gilson Filho <contato@gilsondev.com>
2012-08-18 13:35:38 -0300

    Criando o model Talk

--- src/talks/models.py
+++ src/talks/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Talk(models.Model):
+    name = models.CharField(max_length=100)
+    resume = models.TextField()
+    at = models.DateTimeField(auto_now_add=True)
+    talker = models.CharField(max_length=80)
