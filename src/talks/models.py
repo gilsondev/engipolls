8dea794dff0242a27f040712ac3d683a8c81a493
Gilson Filho <contato@gilsondev.com>
2012-08-20 20:41:24 -0300

    Inserindo o campo type_talk

--- src/talks/models.py
+++ src/talks/models.py
@@ -4,10 +4,18 @@ from django.db import models
 
 
 class Talk(models.Model):
+    TALK_CHOICES = (
+        ('P', u'Palestra'),
+        ('M', u'Minicurso'),
+    )
     name = models.CharField(max_length=100)
     resume = models.TextField()
     at = models.DateTimeField(auto_now_add=True)
     talker = models.CharField(max_length=80)
+    type_talk = models.CharField(max_length=1, choices=TALK_CHOICES)
 
     def __unicode__(self):
-        return "Palestra: %s" % (self.name,)
+        if self.type_talk == 'P':
+            return "Palestra: %s" % (self.name,)
+        elif self.type_talk == 'M':
+            return "Minicurso: %s" % (self.name,)
