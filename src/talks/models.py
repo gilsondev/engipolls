714bc18df87105f931e5efa6075b282d47e96380
Gilson Filho <contato@gilsondev.com>
2012-08-20 22:14:07 -0300

    Internacionalizando o modelo Talk

--- src/talks/models.py
+++ src/talks/models.py
@@ -1,18 +1,20 @@
 # -*- coding: utf8 -*-
 
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class Talk(models.Model):
     TALK_CHOICES = (
-        ('P', u'Palestra'),
-        ('M', u'Minicurso'),
+        ('P', _(u'Palestra')),
+        ('M', _(u'Minicurso')),
     )
-    name = models.CharField(max_length=100)
-    resume = models.TextField()
-    at = models.DateTimeField(auto_now_add=True)
-    talker = models.CharField(max_length=80)
-    type_talk = models.CharField(max_length=1, choices=TALK_CHOICES)
+    name = models.CharField(_(u'Nome da Sessão'), max_length=100)
+    resume = models.TextField(_(u'Resumo'))
+    at = models.DateTimeField(_(u'Data da Sessão'), auto_now_add=True)
+    talker = models.CharField(_(u'Palestrante'), max_length=80)
+    type_talk = models.CharField(_(u'Tipo de Sessão'), max_length=1,
+                                 choices=TALK_CHOICES)
 
     def __unicode__(self):
         if self.type_talk == 'P':
