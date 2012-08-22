34b2b4908736f07cdcd61e15839aff5b4184da32
Gilson Filho <contato@gilsondev.com>
2012-08-21 21:21:53 -0300

    Corrigindo URL

--- src/urls.py
+++ src/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url('^palestras/$', 'src.talks.views.home', name='home'),
+    url('^$', 'src.talks.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
